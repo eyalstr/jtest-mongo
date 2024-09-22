2921023c54bf5ee6e19d5366e469ebf0e34626ae
eyalstr33 <estrauss33@outlook.com>
2024-09-22 22:39:57 +0300

    update response model

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   testEnvironment: 'node', // Jest uses the Node.js environment
   verbose: true,           // Detailed test output
   coverageDirectory: 'coverage', // Generate code coverage reports
+  testTimeout: 10000, // Set to 10 seconds or more
 };
+
+
